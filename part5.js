9d2608d8177700f99dcedabd58168c52474dab39
VitorReis01 <vitor.reis.benites@gmail.com>
2025-05-22 22:15:05 -0300

    desafio return feito com sucesso

--- part5.js
+++ part5.js
@@ -1,22 +1,37 @@
-const cart = [10, 244, 99, 2, 20, 33]
+const cart = [10, 244, 99, 2, 20, 33, 250]
+const valuetotal = [10, 244, 99, 2, 20, 33, 250]
+let finalvalue = 0
+let totalvalue = 0
 
-let discount = 0
-let total = 0
+valuetotal.forEach(value2 => {
+    totalvalue += value2
 
-for (const value of cart) {
+});
+
+function CalculateDiscount(price, discount) {
+    const result = (price * discount) / 100
+
+    return result
+
+}
+
+CalculateDiscount(33, 10)
+
+
+
+
+cart.forEach((value) => {
+
     if (value > 30) {
-        discont = discont + calcularDesconto(value, 10)
-        total = total + value
-    } else {
-        total = total + value
+        const discount = CalculateDiscount(value, 10)
+        finalvalue = finalvalue + (value - discount)
+
     }
-
-    console.log(o valor de suas compras é R$ ${ total }, Você teve compras acima de R$30,00 e recebeu desconto de R$ ${ discont.tofixed(2) }Então o valor a pagar é R$ $ {(total - discont).toFixed(2)}.  )
-
-function calcularDesconto(value, percentual) {
-    return value * percentual / 100
-}
+    else { finalvalue += value }
 
 
 
-}
+
+});
+
+console.log(`o valor final da sua compra foi de R$ ${totalvalue} porém você teve desconto de 10% e pagara sómente R$ ${finalvalue}`)
