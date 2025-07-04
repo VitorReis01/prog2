ea4b3e11f79689bcc1fcc503c3663fde8d8599d8
VitorReis01 <vitor.reis.benites@gmail.com>
2025-07-03 22:31:56 -0300

    ts 1

--- projeto jokenpo/script.js
+++ projeto jokenpo/script.js
@@ -1,5 +1,9 @@
 const result = document.querySelector('.result')
+const scoreHuman = document.querySelector('#score-human')
+const scoreMachine = document.querySelector('#score-machine')
 
+let scoreHumanNumber = 0
+let scoreMachineNumber = 0
 
 const playhuman = (humanChoice) => {
 
@@ -22,13 +26,34 @@ const playTheGame = (human, machine) => {
     console.log('humano:' + human + 'maquina:' + machine)
 
     if (human === machine) {
-        result.innerHTML = "deu empate!"
+        result.innerHTML = "deu empate! affff  &#x1F62B;"
     }
-    else if (human === 'paper' && machine === 'rock') || (human === 'scissors' && machine === 'paper') || (human === 'rock' && machine === 'scissors')
-    { result.innerHTML = "você ganhou!" }
+    else if (
+        (human === 'paper' && machine === 'rock') ||
+        (human === 'scissors' && machine === 'paper') ||
+        (human === 'rock' && machine === 'scissors')
+
+
+
+    ) {
+        scoreHumanNumber++
+        scoreHuman.innerHTML = scoreHumanNumber
+        result.innerHTML = "você ganhou! 'fez mais que sua obrigação!!! &#x1F644;"
+        scoreHumanNumber === 20 && scoreMachineNumber < 20, result.innerHTML = "parabens você esta batendo na maquina &#x1F633;"
 
     else {
-    result.innerHTML = "você perdeu para a maquina"
+        scoreMachineNumber++
+        scoreMachine.innerHTML = scoreMachineNumber
+        result.innerHTML = "você perdeu para a maquina &#x1F92A;"
+        scoreMachineNumber === 20 && scoreHumanNumber < 20, result.innerHTML = "a maquina esta ta dando uma surra &#x1F923; "
+
+    }
+
+
+
 }
-}
+
+
+
+
 
